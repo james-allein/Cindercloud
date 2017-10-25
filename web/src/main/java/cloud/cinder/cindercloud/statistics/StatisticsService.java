2842e4fca157d13a61f4decc93b8dc07c97c622b
Quinten De Swaef <quintendeswaef@gmail.com>
2017-10-25 21:26:27 +0200

    start of gh-2

--- web/src/main/java/cloud/cinder/cindercloud/statistics/StatisticsService.java
+++ web/src/main/java/cloud/cinder/cindercloud/statistics/StatisticsService.java
@@ -1,4 +1,38 @@
 package cloud.cinder.cindercloud.statistics;
 
+import cloud.cinder.cindercloud.block.model.Block;
+import cloud.cinder.cindercloud.block.repository.BlockRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.math.BigInteger;
+import java.time.LocalDateTime;
+import java.time.OffsetDateTime;
+import java.time.ZoneOffset;
+import java.time.temporal.TemporalAmount;
+import java.util.Date;
+import java.util.stream.Stream;
+
+@Service
 public class StatisticsService {
+
+    @Autowired
+    private BlockRepository blockRepository;
+
+    public double getAverageDifficulty(final TemporalAmount since) {
+        final OffsetDateTime dateSince = LocalDateTime.now().atOffset(ZoneOffset.UTC).minus(since);
+        final Stream<Block> allSince = blockRepository.findAllBlocksSince(Date.from(dateSince.toInstant()));
+        return allSince
+                .map(Block::getDifficulty)
+                .mapToDouble(BigInteger::doubleValue)
+                .average().orElse(0);
+    }
+
+//    public double getAverageTransactions(final TemporalAmount since) {
+//
+//    }
+//
+//    public double getAverageBlockTime(final TemporalAmount since) {
+//
+//    }
 }
