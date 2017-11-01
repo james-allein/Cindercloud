1d2d782625756be0612669a52a041a2142a98198
Quinten De Swaef <quintendeswaef@gmail.com>
2017-11-01 21:43:58 +0100

    fix access key problems

--- web/src/main/java/cloud/cinder/cindercloud/config/AWSConfiguration.java
+++ web/src/main/java/cloud/cinder/cindercloud/config/AWSConfiguration.java
@@ -6,12 +6,14 @@ import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 
 @Configuration
 public class AWSConfiguration {
 
     @Bean
-    AWSCredentialsProvider credProvider(@Value("${cloud.aws.credentials.accessKeyId") final String id,
+    @Primary
+    AWSCredentialsProvider credProvider(@Value("${cloud.aws.credentials.accessKeyId}") final String id,
                                         @Value("${cloud.aws.credentials.secretKey}") final String secret) {
         return new AWSStaticCredentialsProvider(new AWSCredentials() {
             @Override
