f7c844786278da861b723fa166c3198a92e46b4c
Quinten De Swaef <quintendeswaef@gmail.com>
2017-11-06 17:40:45 +0100

    added race condition

--- web/src/main/java/cloud/cinder/cindercloud/block/continuous/BlockImportJobRunner.java
+++ web/src/main/java/cloud/cinder/cindercloud/block/continuous/BlockImportJobRunner.java
@@ -1,12 +1,17 @@
 package cloud.cinder.cindercloud.block.continuous;
 
+import cloud.cinder.cindercloud.block.continuous.model.BlockImportJob;
 import cloud.cinder.cindercloud.block.continuous.repository.BlockImportJobRepository;
+import com.google.common.collect.Lists;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.collections.ListUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.stream.Stream;
 
 @Component
@@ -22,8 +27,12 @@ public class BlockImportJobRunner {
     @Scheduled(fixedDelay = 10000)
     public void run() {
         log.info("Starting Block Import Job Runner");
-        Stream.concat(blockImportJobRepository.findAllActive().stream(), blockImportJobRepository.findAllNotEndedYet().stream())
-                .forEach(x -> blockImporter.execute(x));
+        final List<BlockImportJob> allActive = blockImportJobRepository.findAllActive();
+        final List<BlockImportJob> allNotEndedYet = blockImportJobRepository.findAllNotEndedYet();
+        List<BlockImportJob> union = ListUtils.union(allActive, allNotEndedYet);
+        if (union.size() > 0) {
+            blockImporter.execute(union.get(0));
+        }
         log.info("Done with block runner");
     }
 
