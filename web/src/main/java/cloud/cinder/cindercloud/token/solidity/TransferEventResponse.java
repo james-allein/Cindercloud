91db752f76bdb6461a2e808c81fe99e0613bd437
Quinten De Swaef <quintendeswaef@gmail.com>
2017-11-12 18:55:51 +0100

    added new humanstandard token, removed old things we don't need

---
+++ web/src/main/java/cloud/cinder/cindercloud/token/solidity/TransferEventResponse.java
@@ -0,0 +1,28 @@
+package cloud.cinder.cindercloud.token.solidity;
+
+import java.math.BigInteger;
+
+public class TransferEventResponse {
+
+    private String from;
+    private String to;
+    private BigInteger value;
+
+    public TransferEventResponse(final String from, final String to, final BigInteger value) {
+        this.from = from;
+        this.to = to;
+        this.value = value;
+    }
+
+    public String getFrom() {
+        return from;
+    }
+
+    public String getTo() {
+        return to;
+    }
+
+    public BigInteger getValue() {
+        return value;
+    }
+}
