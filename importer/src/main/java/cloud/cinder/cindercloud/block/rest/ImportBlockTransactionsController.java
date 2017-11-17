ce9dddf288621ccbabee5c78ade4157d25d0c839
Quinten De Swaef <quintendeswaef@gmail.com>
2017-11-17 10:09:55 +0100

    We can now use EB-workers

---
+++ importer/src/main/java/cloud/cinder/cindercloud/block/rest/ImportBlockTransactionsController.java
@@ -0,0 +1,37 @@
+package cloud.cinder.cindercloud.block.rest;
+
+import cloud.cinder.cindercloud.block.model.Block;
+import cloud.cinder.cindercloud.transaction.continuous.TransactionImporter;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/import/block/transactions")
+@Slf4j
+public class ImportBlockTransactionsController {
+
+    @Autowired
+    private TransactionImporter transactionImporter;
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @RequestMapping(method = RequestMethod.POST)
+    public ResponseEntity<Void> importTransactions(@RequestBody final String blockAsString) {
+        try {
+            log.debug("Fetched from queue: {}", blockAsString);
+            final Block convertedBlock = objectMapper.readValue(blockAsString, Block.class);
+            transactionImporter.importTransactions(convertedBlock);
+            return new ResponseEntity<>(HttpStatus.OK);
+        } catch (final Exception ex) {
+            log.error("Error trying to receive message", ex);
+            throw new IllegalArgumentException("Unable to process");
+        }
+    }
+}
