f3df4960a5adb83f5cabe77d93f9388b0228af51
Quinten De Swaef <quintendeswaef@gmail.com>
2017-11-27 23:10:09 +0100

    Leaked credentials will be saved to the database

---
+++ common/src/main/java/cloud/cinder/cindercloud/credential/domain/LeakedCredential.java
@@ -0,0 +1,29 @@
+package cloud.cinder.cindercloud.credential.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Table(name = "leaked_credentials")
+@Entity
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class LeakedCredential {
+
+    @Column(name = "address")
+    @Id
+    private String address;
+
+    @Column(name = "private_key")
+    private String privateKey;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "date_added")
+    private Date dateAdded;
+}
