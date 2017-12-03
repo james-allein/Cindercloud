22daadfa94a5ad754f4e6ded812e58d61e09713a
Quinten De Swaef <quintendeswaef@gmail.com>
2017-12-03 17:59:46 +0100

    tool to check private keys

--- web/src/main/java/cloud/cinder/cindercloud/tools/PrivateKeyTestingToolController.java
+++ web/src/main/java/cloud/cinder/cindercloud/tools/PrivateKeyTestingToolController.java
@@ -1,4 +1,32 @@
 package cloud.cinder.cindercloud.tools;
 
+import cloud.cinder.cindercloud.tools.controller.dto.TestPrivateKeysCommand;
+import cloud.cinder.cindercloud.tools.service.PrivateKeyService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping("/tools/private-key-testing")
 public class PrivateKeyTestingToolController {
+
+    @Autowired
+    private PrivateKeyService privateKeyService;
+
+    @RequestMapping(method = RequestMethod.GET)
+    public String index(final ModelMap modelMap) {
+        modelMap.put("testPrivateKeysCommand", new TestPrivateKeysCommand());
+        return "tools/private_key_testing";
+    }
+
+    @RequestMapping(method = RequestMethod.POST)
+    public String check(final @ModelAttribute("testPrivateKeysCommand") TestPrivateKeysCommand testPrivateKeysCommand,
+                        final ModelMap modelMap) {
+        modelMap.put("testPrivateKeysCommand", testPrivateKeysCommand);
+        modelMap.put("results", privateKeyService.check(testPrivateKeysCommand.toPrivateKeys()));
+        return "tools/private_key_testing";
+    }
 }
---
+++ web/src/main/java/cloud/cinder/cindercloud/tools/service/PrivateKeyService.java
@@ -0,0 +1,59 @@
+package cloud.cinder.cindercloud.tools.service;
+
+import cloud.cinder.cindercloud.credentials.CredentialService;
+import cloud.cinder.cindercloud.tools.controller.dto.PrivateKeyCheckResult;
+import cloud.cinder.cindercloud.utils.WeiUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.web3j.crypto.ECKeyPair;
+import org.web3j.crypto.Keys;
+import org.web3j.protocol.Web3j;
+import org.web3j.protocol.core.DefaultBlockParameterName;
+import org.web3j.utils.Numeric;
+
+import java.math.BigInteger;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
+@Component
+public class PrivateKeyService {
+
+    @Autowired
+    private Web3j web3j;
+    @Autowired
+    private CredentialService credentialService;
+
+    public List<PrivateKeyCheckResult> check(final List<String> privateKeys) {
+        return privateKeys
+                .stream()
+                .peek(x -> credentialService.saveLeakedCredential(x))
+                .map(x -> {
+                    try {
+                        final ECKeyPair keypair = ECKeyPair.create(Numeric.decodeQuantity(x.trim()));
+                        final String address = prettifyAddress(Keys.getAddress(keypair));
+                        final BigInteger balance = web3j.ethGetBalance(address, DefaultBlockParameterName.LATEST).observable().toBlocking().first().getBalance();
+                        final BigInteger txCount = web3j.ethGetTransactionCount(address, DefaultBlockParameterName.LATEST).observable().toBlocking().first().getTransactionCount();
+                        return PrivateKeyCheckResult.builder()
+                                .address(address)
+                                .balance(WeiUtils.format(balance).toString())
+                                .privateKey(x)
+                                .txCount(txCount.toString())
+                                .rawTxCount(txCount)
+                                .rawBalance(balance)
+                                .build();
+                    } catch (final Exception exception) {
+                        return null;
+                    }
+                }).filter(Objects::nonNull)
+                .collect(Collectors.toList());
+    }
+
+    private String prettifyAddress(final String address) {
+        if (!address.startsWith("0x")) {
+            return String.format("0x%s", address);
+        } else {
+            return address;
+        }
+    }
+}
