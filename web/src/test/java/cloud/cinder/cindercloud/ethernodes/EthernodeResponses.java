0b16b95365c7c25fe82e631cc4aef8336a7ea823
Quinten De Swaef <quintendeswaef@gmail.com>
2017-12-04 20:46:27 +0100

    fix

---
+++ web/src/test/java/cloud/cinder/cindercloud/ethernodes/EthernodeResponses.java
@@ -0,0 +1,4 @@
+package cloud.cinder.cindercloud.ethernodes;
+
+public class EthernodeResponses {
+}
---
+++ web/src/test/java/cloud/cinder/cindercloud/ethernodes/Node.java
@@ -0,0 +1,13 @@
+package cloud.cinder.cindercloud;
+
+import java.util.Date;
+
+public class Node {
+    private String id;
+    private String host;
+    private String port;
+    private String clientId;
+    private String client;
+    private Date lastUpdate;
+    private String country;
+}
