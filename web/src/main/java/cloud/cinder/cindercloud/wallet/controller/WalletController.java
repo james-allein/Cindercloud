cf67fddb9d5378eab423853e21906f91354de730
Quinten De Swaef <quintendeswaef@gmail.com>
2017-12-08 16:41:28 +0100

    Able to get transactions now from wallet

--- web/src/main/java/cloud/cinder/cindercloud/wallet/controller/WalletController.java
+++ web/src/main/java/cloud/cinder/cindercloud/wallet/controller/WalletController.java
@@ -1,9 +1,14 @@
 package cloud.cinder.cindercloud.wallet.controller;
 
 import cloud.cinder.cindercloud.address.service.AddressService;
+import cloud.cinder.cindercloud.coinmarketcap.dto.Currency;
+import cloud.cinder.cindercloud.coinmarketcap.service.PriceService;
 import cloud.cinder.cindercloud.security.domain.PrivateKeyAuthentication;
+import cloud.cinder.cindercloud.transaction.service.TransactionService;
 import cloud.cinder.cindercloud.utils.WeiUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
@@ -11,13 +16,20 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import java.math.BigInteger;
+import java.text.DecimalFormat;
 
 @Controller
 @RequestMapping("/wallet")
 public class WalletController {
 
+    private static final DecimalFormat currencyFormat = new DecimalFormat("#.00");
+
     @Autowired
     private AddressService addressService;
+    @Autowired
+    private PriceService priceService;
+    @Autowired
+    private TransactionService transactionService;
 
     @RequestMapping(method = RequestMethod.GET)
     public String index(final ModelMap modelMap) {
@@ -25,9 +37,24 @@ public class WalletController {
         final String address = (String) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
         final BigInteger balance = addressService.getBalance(address).toBlocking().first();
         modelMap.put("balance", WeiUtils.format(balance));
+        modelMap.put("balEUR", formatCurrency(priceService.getPrice(Currency.EUR) * WeiUtils.asEth(balance)));
+        modelMap.put("balUSD", formatCurrency(priceService.getPrice(Currency.USD) * WeiUtils.asEth(balance)));
         modelMap.put("hasBalance", balance.longValue() != 0);
         modelMap.put("address", address);
         return "wallets/index";
+    }
+
+    @RequestMapping(method = RequestMethod.GET, value = "/transactions")
+    public String transactions(final ModelMap modelMap,
+                               final Pageable pageable) {
+        requireAuthenticated();
+        final String address = (String) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        modelMap.put("transactions", transactionService.findByAddress(address, new PageRequest(0, 25)).toBlocking().first());
+        return "wallets/transactions";
+    }
+
+    private String formatCurrency(final double v) {
+        return currencyFormat.format(v);
     }
 
     private void requireAuthenticated() {
