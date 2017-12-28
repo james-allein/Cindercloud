e210705f9e5e0141fdc7909307345529fc46ce11
Quinten De Swaef <quintendeswaef@gmail.com>
2017-12-28 04:37:25 +0100

    more tokens, upgrade of web3

---
+++ web/src/main/resources/db/migration/V17__more_tokens.sql
@@ -0,0 +1,43 @@
+INSERT INTO tokens (name, slug, address, website, social, symbol, decimals, coinmarketcap_name)
+VALUES ('Everex', 'everex', '0xf3db5fa2c66b7af3eb0c0b782510816cbe4813b8', 'https://www.everex.io/',
+        'https://twitter.com/everexio', 'EVX', 4, 'everex');
+
+INSERT INTO tokens (name, slug, address, website, social, symbol, decimals, coinmarketcap_name)
+VALUES ('FunFair', 'funfair', '0x419d0d8bdd9af5e606ae2232ed285aff190e711b', 'https://funfair.io/',
+        'https://twitter.com/FunFairTech', 'FUN', 8, 'funfair');
+
+INSERT INTO tokens (name, slug, address, website, social, symbol, decimals, coinmarketcap_name)
+VALUES ('Qtum', 'qtum', '0x9a642d6b3368ddc662CA244bAdf32cDA716005BC', 'https://qtum.org/',
+        'https://twitter.com/QtumOfficial', 'QTUM', 18, 'qtum');
+
+
+INSERT INTO tokens (name, slug, address, website, social, symbol, decimals, coinmarketcap_name)
+VALUES ('Santiment', 'santiment', '0x7c5a0ce9267ed19b22f8cae653f198e3e8daf098', 'https://santiment.net/',
+        'https://twitter.com/santimentfeed', 'SAN', 18, 'santiment');
+
+INSERT INTO tokens (name, slug, address, website, social, symbol, decimals, coinmarketcap_name)
+VALUES ('Kyber Network Crystal', 'kyber_network_crystal', '0xdd974d5c2e2928dea5f71b9825b8b646686bd200',
+        'https://kyber.network/',
+        'https://twitter.com/KyberNetwork', 'KNC', 18, 'kyber-network');
+
+INSERT INTO tokens (name, slug, address, website, social, symbol, decimals, coinmarketcap_name)
+VALUES ('0xProject', '0xproject', '0xe41d2489571d322189246dafa5ebde1f4699f498', 'https://0xproject.com/',
+        'https://twitter.com/0xproject', 'ZRX', 18, '0x');
+
+INSERT INTO tokens (name, slug, address, website, social, symbol, decimals, coinmarketcap_name)
+VALUES ('Civic', 'civic', '0xe41d2489571d322189246dafa5ebde1f4699f498', 'https://www.civic.com/',
+        'https://twitter.com/civickey', 'CVC', 8, 'civic');
+
+
+INSERT INTO tokens (name, slug, address, website, social, symbol, decimals, coinmarketcap_name)
+VALUES ('Iconomi', 'iconomi', '0x888666CA69E0f178DED6D75b5726Cee99A87D698', 'https://iconomi.net/',
+        'https://twitter.com/iconominet', 'ICN', 18, 'iconomi');
+
+INSERT INTO tokens (name, slug, address, website, social, symbol, decimals, coinmarketcap_name)
+VALUES ('Gnosis', 'gnosis', '0x6810e776880c02933d47db1b9fc05908e5386b96', 'https://gnosis.pm/',
+        'https://twitter.com/gnosisPM', 'GNO', 18, 'gnosis-go');
+
+INSERT INTO tokens (name, slug, address, website, social, symbol, decimals, coinmarketcap_name)
+VALUES ('Monaco', 'monaco', '0xb63b606ac810a52cca15e44bb630fd42d8d1d83d', 'http://www.mona.co/',
+        'https://twitter.com/monaco_card', 'GNO', 8, 'monaco');
+
