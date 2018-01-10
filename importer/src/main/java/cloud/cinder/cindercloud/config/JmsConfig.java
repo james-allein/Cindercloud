0c0a9b384821b7821458435329dcb5f6b7b8f990
Quinten De Swaef <quintendeswaef@gmail.com>
2018-01-10 21:11:11 +0100

    fix production issue

--- importer/src/main/java/cloud/cinder/cindercloud/config/JmsConfig.java
+++ importer/src/main/java/cloud/cinder/cindercloud/config/JmsConfig.java
@@ -5,6 +5,7 @@ import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.listener.SimpleMessageListenerContainer;
 import org.springframework.amqp.rabbit.listener.adapter.MessageListenerAdapter;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -12,6 +13,7 @@ import org.springframework.context.annotation.Configuration;
 public class JmsConfig {
 
     @Bean
+    @ConditionalOnProperty(name = "cloud.cinder.ethereum.sqs-transaction-import", havingValue = "true")
     SimpleMessageListenerContainer container(ConnectionFactory connectionFactory,
                                              MessageListenerAdapter listenerAdapter,
                                              @Value("${cloud.cinder.sqs.name}") final String queueName) {
@@ -23,6 +25,7 @@ public class JmsConfig {
     }
 
     @Bean
+    @ConditionalOnProperty(name = "cloud.cinder.ethereum.sqs-transaction-import", havingValue = "true")
     MessageListenerAdapter listenerAdapter(BlockAddedListener receiver) {
         return new MessageListenerAdapter(receiver, "receiveMessage");
     }
