3763983aed309f442bf3d73de845a27143402d02
Quinten De Swaef <quintendeswaef@gmail.com>
2018-01-12 16:34:46 +0100

    new version

--- whitehat/src/main/java/cloud/cinder/cindercloud/sweeping/continuous/EthereumSweeperService.java
+++ whitehat/src/main/java/cloud/cinder/cindercloud/sweeping/continuous/EthereumSweeperService.java
@@ -2,8 +2,13 @@ package cloud.cinder.cindercloud.sweeping.continuous;
 
 import cloud.cinder.cindercloud.credentials.service.CredentialService;
 import cloud.cinder.cindercloud.sweeping.EthereumSweeper;
+import com.codahale.metrics.MetricRegistry;
+import com.codahale.metrics.Timer;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.actuate.metrics.CounterService;
+import org.springframework.boot.actuate.metrics.GaugeService;
+import org.springframework.boot.actuate.metrics.dropwizard.DropwizardMetricServices;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
@@ -16,11 +21,24 @@ public class EthereumSweeperService {
     private EthereumSweeper ethereumSweeper;
     @Autowired
     private CredentialService leakedCredentialRepository;
+    private Timer jobtimer;
+
+
+    @Autowired
+    public EthereumSweeperService(MetricRegistry metricRegistry) {
+        this.jobtimer = metricRegistry.timer("sweeper");
+    }
 
     @Scheduled(fixedDelay = 1000)
     public void sweepKnownAddresses() {
-        leakedCredentialRepository.streamAll()
-                .forEach(x -> ethereumSweeper.sweep(x.getPrivateKey()));
+        Timer.Context timer = jobtimer.time();
+        try {
+            leakedCredentialRepository.streamAll()
+                    .forEach(x -> ethereumSweeper.sweep(x.getPrivateKey()));
+            timer.stop();
+        } catch (final Exception ex) {
+            log.debug("Exception during execution of sweeper-timer");
+        }
     }
 
     @Async
