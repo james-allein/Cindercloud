36799818b556644a297c10b09a7b3ca667ff35d1
Quinten De Swaef <quintendeswaef@gmail.com>
2018-03-11 00:33:33 +0100

    created a way to switch accounts

---
+++ web/src/main/java/cloud/cinder/cindercloud/wallet/controller/LogoutController.java
@@ -0,0 +1,19 @@
+package cloud.cinder.cindercloud.wallet.controller;
+
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
+public class LogoutController {
+
+    @GetMapping("/logout")
+    public String logout(final HttpServletRequest httpServletRequest) {
+        SecurityContextHolder.getContext().getAuthentication().setAuthenticated(false);
+        httpServletRequest.getSession(false).invalidate();
+        return "redirect:/wallet/login";
+    }
+
+}
