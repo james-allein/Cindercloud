d52c73d9c51be52a068ff07f074df117d97036f2
Quinten De Swaef <quintendeswaef@gmail.com>
2018-03-11 02:16:19 +0100

    Enabling webflux

---
+++ web/src/main/java/cloud/cinder/cindercloud/config/WebConfiguration.java
@@ -0,0 +1,9 @@
+package cloud.cinder.cindercloud.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.config.EnableWebFlux;
+
+@Configuration
+@EnableWebFlux
+public class WebConfiguration {
+}
