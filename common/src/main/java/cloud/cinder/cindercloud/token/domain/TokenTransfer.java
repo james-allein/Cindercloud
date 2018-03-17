9d5919231a829db29dd98073ac310fe03e8a4190
Quinten De Swaef <quintendeswaef@gmail.com>
2018-03-17 20:50:30 +0100

    token transfers modelé

--- common/src/main/java/cloud/cinder/cindercloud/token/domain/TokenTransfer.java
+++ common/src/main/java/cloud/cinder/cindercloud/token/domain/TokenTransfer.java
@@ -12,15 +12,25 @@ import java.util.Date;
 @Data
 @NoArgsConstructor
 public class TokenTransfer {
-
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Column(name = "from_address")
     private String from;
+    @Column(name = "to_address")
     private String to;
     private BigInteger amount;
-    private String transactionid;
-
+    @Column(name = "transaction_hash")
+    private String transactionHash;
+    @Column(name = "transaction_index")
+    private BigInteger transactionIndex;
+    @Column(name = "origin_address")
+    private String originAddress;
+    @Column(name = "removed")
+    private boolean removed;
     @Column(name = "block_timestamp")
     @Temporal(TemporalType.TIMESTAMP)
     private Date blockTimestamp;
-
+    @Column(name = "token_address")
+    private String tokenAddress;
 }
