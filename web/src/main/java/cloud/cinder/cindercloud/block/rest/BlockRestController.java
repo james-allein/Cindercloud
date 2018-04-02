48fe5d8c20eaada2aacbd4154ecfa720bebdf63b
Quinten De Swaef <quintendeswaef@gmail.com>
2018-04-02 18:12:38 +0200

    cleanup

--- web/src/main/java/cloud/cinder/cindercloud/block/rest/BlockRestController.java
+++ web/src/main/java/cloud/cinder/cindercloud/block/rest/BlockRestController.java
@@ -2,15 +2,20 @@ package cloud.cinder.cindercloud.block.rest;
 
 import cloud.cinder.cindercloud.block.domain.Block;
 import cloud.cinder.cindercloud.block.service.BlockService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Pageable;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.context.request.async.DeferredResult;
 
 import java.util.List;
 
 @RestController
 @RequestMapping(value = "/rest/block")
+@Slf4j
 public class BlockRestController {
 
     @Autowired
@@ -24,7 +29,9 @@ public class BlockRestController {
     @RequestMapping(value = "/{hash}")
     public DeferredResult<Block> getBlock(@PathVariable("hash") final String hash) {
         final DeferredResult<Block> result = new DeferredResult<>();
-        blockService.getBlock(hash).subscribe(result::setResult, Throwable::printStackTrace);
+        blockService.getBlock(hash).subscribe(result::setResult, e -> {
+            log.debug("Unable to fetch block: {}", hash);
+        });
         return result;
     }
 
