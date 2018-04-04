dfa82316eac6979cc035ac4b8f002f8f29102247
Quinten De Swaef <quintendeswaef@gmail.com>
2018-04-04 18:04:13 +0200

    don't import everything you see...

--- importer/src/main/java/cloud/cinder/cindercloud/token/listener/UserTokenImportListener.java
+++ importer/src/main/java/cloud/cinder/cindercloud/token/listener/UserTokenImportListener.java
@@ -6,7 +6,15 @@ import com.codahale.metrics.MetricRegistry;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+import java.time.temporal.ChronoUnit;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 @Component
 @ConditionalOnProperty(name = "token-transfer-queue-import", havingValue = "true")
@@ -16,6 +24,9 @@ public class UserTokenImportListener {
     private ObjectMapper objectMapper;
     private TokenTransferHistoricImporter tokenTransferHistoricImporter;
     private final Meter blockImportMeter;
+
+    private Map<String, Date> alreadySweeped = new HashMap<>();
+
 
     public UserTokenImportListener(final ObjectMapper objectMapper,
                                    final MetricRegistry metricRegistry,
@@ -30,12 +41,27 @@ public class UserTokenImportListener {
         try {
             log.debug("Fetched from queue: {}", userTokenImportRequestAsString);
             final UserTokenRequest userTokenRequest = objectMapper.readValue(userTokenImportRequestAsString, UserTokenRequest.class);
-            tokenTransferHistoricImporter.importForUserTokens(userTokenRequest);
+            alreadySweeped.computeIfAbsent(userTokenRequest.getAddress(), s -> {
+                tokenTransferHistoricImporter.importForUserTokens(userTokenRequest);
+                return new Date();
+            });
         } catch (final Exception ex) {
             log.error("Error trying to receive message", ex);
             throw new IllegalArgumentException("Unable to process");
         } finally {
             blockImportMeter.mark();
         }
+    }
+
+    @Scheduled(fixedDelay = 10000)
+    public void removeAgedAlreadySweeped() {
+        final HashMap<String, Date> hashmapCopy = new HashMap<>(alreadySweeped);
+        hashmapCopy.forEach((address, date) -> {
+            final Date oneDayAgo = Date.from(LocalDateTime.now().toInstant(ZoneOffset.UTC).minus(1, ChronoUnit.DAYS));
+            if (date.before(oneDayAgo)) {
+                log.debug("Removing aged finding: {}", address);
+                alreadySweeped.remove(address);
+            }
+        });
     }
 }
