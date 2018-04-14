671ed8d9e5337e742ece9bc4c665c42244a2dbd7
Quinten De Swaef <quintendeswaef@gmail.com>
2018-04-14 19:55:19 +0200

    Adding tokens which are supported by Kyber

---
+++ common/src/main/resources/db/migration/V32__kyber_tokens.sql
@@ -0,0 +1,39 @@
+INSERT INTO tokens (name, slug, address, website, social, symbol, decimals, coinmarketcap_name)
+VALUES ('PowerLedger', 'powerledger', '0x595832f8fc6bf59c85c527fec3740a1b7a361269', 'https://powerledger.io/',
+        'https://twitter.com/PowerLedger_io', 'POWR', 6, 'power-ledger');
+
+INSERT INTO tokens (name, slug, address, website, social, symbol, decimals, coinmarketcap_name)
+VALUES ('Decentraland', 'decentraland', '0x0f5d2fb29fb7d3cfee444a200298f468908cc942', 'https://decentraland.org/',
+        'https://twitter.com/decentraland', 'MANA', 18, 'decentraland');
+
+INSERT INTO tokens (name, slug, address, website, social, symbol, decimals, coinmarketcap_name)
+VALUES ('Gifto', 'gifto', '0xc5bbae50781be1669306b9e001eff57a2957b09d', 'https://gifto.io/',
+        'https://twitter.com/GIFTO_io', 'GTO', 5, 'gifto');
+
+INSERT INTO tokens (name, slug, address, website, social, symbol, decimals, coinmarketcap_name)
+VALUES ('Raiden', 'raiden', '0x255aa6df07540cb5d3d297f0d0d4d84cb52bc8e6', 'https://raiden.network/',
+        'https://twitter.com/raiden_network', 'RDN', 18, 'raiden-network-token');
+
+
+INSERT INTO tokens (name, slug, address, website, social, symbol, decimals, coinmarketcap_name)
+VALUES ('AppCoins', 'appcoins', '0x1a7a8bd9106f2b8d977e08582dc7d24c723ab0db', 'https://appcoins.io/',
+        'https://twitter.com/AppCoinsProject', 'APPC', 18, 'appcoins');
+
+INSERT INTO tokens (name, slug, address, website, social, symbol, decimals, coinmarketcap_name)
+VALUES ('Enigma', 'enigma', '0xf0ee6b27b759c9893ce4f094b49ad28fd15a23e4', 'https://enigma.co/',
+        'https://twitter.com/enigmampc', 'ENG', 8, 'appcoins');
+
+INSERT INTO tokens (name, slug, address, website, symbol, decimals, coinmarketcap_name)
+VALUES ('AdEx', 'adex', '0x4470BB87d77b963A013DB939BE332f927f2b992e', 'https://www.adex.network/', 'ADX', 4, 'adx-net');
+
+INSERT INTO tokens (name, slug, address, website, social, symbol, decimals, coinmarketcap_name)
+VALUES ('AirSwap', 'airswap', '0x27054b13b1b798b345b591a4d22e6562d47ea75a', 'https://www.airswap.io/',
+        'https://twitter.com/airswap', 'AST', 4, 'airswap');
+
+INSERT INTO tokens (name, slug, address, website, social, symbol, decimals, coinmarketcap_name)
+VALUES ('RipioCreditNetwork', 'ripio_credit_network', '0xf970b8e36e23f7fc3fd752eea86f8be8d83375a6', 'https://ripiocredit.network/',
+        'https://twitter.com/RCN_token', 'RCN', 18, 'ripio-credit-network');
+
+INSERT INTO tokens (name, slug, address, website, social, symbol, decimals, coinmarketcap_name)
+VALUES ('ChainLink Token', 'chainlink', '0x514910771af9ca656af840dff83e8264ecf986ca', 'https://link.smartcontract.com/',
+        'https://twitter.com/smart_contract', 'LINK', 18, 'chainlink');
