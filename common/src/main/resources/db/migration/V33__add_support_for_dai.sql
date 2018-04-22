9ea3f91fa855798205ba73fd267f4b26e048861f
Quinten De Swaef <quintendeswaef@gmail.com>
2018-04-22 12:23:49 +0200

    DAI was added

---
+++ common/src/main/resources/db/migration/V33__add_support_for_dai.sql
@@ -0,0 +1,3 @@
+INSERT INTO tokens (name, slug, address, website, social, symbol, decimals, coinmarketcap_name)
+VALUES ('Dai', 'dai', '0x89d24a6b4ccb1b6faa2625fe562bdd9a23260359', 'https://makerdao.com/',
+        'https://twitter.com/MakerDAO', 'DAI', 18, 'dai');
