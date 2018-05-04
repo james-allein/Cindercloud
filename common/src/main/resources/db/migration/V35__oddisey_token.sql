ca3cebcc00fca6fdd0855a3103b52e886cd3ac29
Quinten De Swaef <quintendeswaef@gmail.com>
2018-05-04 15:04:22 +0200

    oddisey token

---
+++ common/src/main/resources/db/migration/V35__oddisey_token.sql
@@ -0,0 +1,3 @@
+INSERT INTO tokens (name, slug, address, website, social, symbol, decimals, coinmarketcap_name)
+VALUES ('Odyssey', 'odyssey', '0x4092678e4E78230F46A1534C0fbc8fA39780892B', 'http://www.ocoins.cc/home/index.php',
+        'https://twitter.com/ODYSSEYPROTOCOL', 'OCN', 18, 'odyssey');
