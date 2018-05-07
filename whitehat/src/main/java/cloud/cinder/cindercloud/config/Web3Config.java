d9de8f5ff24cd5627a027c1d330cad5796317410
Quinten De Swaef <quintendeswaef@gmail.com>
2018-05-07 22:47:07 +0200

    gonna try these executors

--- whitehat/src/main/java/cloud/cinder/cindercloud/config/Web3Config.java
+++ whitehat/src/main/java/cloud/cinder/cindercloud/config/Web3Config.java
@@ -8,7 +8,8 @@ import org.springframework.context.annotation.Primary;
 import org.web3j.protocol.Web3j;
 import org.web3j.protocol.Web3jService;
 import org.web3j.protocol.http.HttpService;
-import org.web3j.protocol.infura.InfuraHttpService;
+
+import java.util.concurrent.Executors;
 
 @Configuration
 public class Web3Config {
@@ -16,13 +17,15 @@ public class Web3Config {
     @Bean
     @Primary
     public Web3j provideWeb3J(final Web3jService web3jService) {
-        return Web3j.build(web3jService);
+        return Web3j.build(web3jService,
+                15 * 1000, Executors.newScheduledThreadPool(30));
     }
 
     @Bean
     @Qualifier("local")
-    public Web3j provideInfuraWeb3j(@Qualifier("local") final Web3jService web3jService) {
-        return Web3j.build(web3jService);
+    public Web3j provideLocalWeb3(@Qualifier("local") final Web3jService web3jService) {
+        return Web3j.build(web3jService,
+                15 * 1000, Executors.newScheduledThreadPool(30));
     }
 
     @Bean
