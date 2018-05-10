7c458d2ad01bde02d1b828c0c94105fea01ee8c3
Quinten De Swaef <quintendeswaef@gmail.com>
2018-05-10 16:48:21 +0200

    echolink added

---
+++ common/src/main/resources/db/migration/V37__echolink_as_token.sql
@@ -0,0 +1,3 @@
+INSERT INTO tokens (name, slug, address, website, social, symbol, decimals, coinmarketcap_name)
+VALUES ('EchoLink', 'echolink', '0xa6a840e50bcaa50da017b91a0d86b8b2d41156ee', 'https://echolink.info/',
+        'https://twitter.com/EchoLinkInfo1', 'EKO', 18, 'echolink');
