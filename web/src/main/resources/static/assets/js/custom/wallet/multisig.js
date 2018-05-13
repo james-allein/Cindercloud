f433f19ec0bac6fccd38b7e1fced6a63ed45628f
Quinten De Swaef <quintendeswaef@gmail.com>
2018-05-13 15:18:06 +0200

    multisig support

--- web/src/main/resources/static/assets/js/custom/wallet/multisig.js
+++ web/src/main/resources/static/assets/js/custom/wallet/multisig.js
@@ -1,3 +1,14 @@
 (function () {
 
+	const address = $('#currentAddress').data('hash');
+	const multisigAddress = $('#multisigAddress').data('hash');
+
+	let multisigData = {
+
+	};
+
+	new Vue({
+		el: '#multisigApp',
+		data: multisigData
+	});
 })();
