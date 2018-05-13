8b2521e024480cdc7edf3183d563a7c65c4b8ceb
Quinten De Swaef <quintendeswaef@gmail.com>
2018-05-13 16:18:24 +0200

    arcane setting up

--- arcane/src/main/java/cloud/cinder/cindercloud/arcane/ArcaneApplication.java
+++ arcane/src/main/java/cloud/cinder/cindercloud/arcane/ArcaneApplication.java
@@ -1,4 +1,25 @@
 package cloud.cinder.cindercloud.arcane;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
+
+import java.net.UnknownHostException;
+
+@Slf4j
+@EnableAutoConfiguration
+@SpringBootApplication
 public class ArcaneApplication {
+
+    public static void main(String[] args) throws UnknownHostException {
+        final SpringApplication app = new SpringApplication(ArcaneApplication.class);
+        final Environment env = app.run(args).getEnvironment();
+        log.info("\n----------------------------------------------------------\n\t"
+                        + "Application '{}' is running! Access URLs:\n\t"
+                        + "Profile(s): \t{}\n----------------------------------------------------------",
+                env.getProperty("spring.application.name"),
+                env.getActiveProfiles());
+    }
 }
