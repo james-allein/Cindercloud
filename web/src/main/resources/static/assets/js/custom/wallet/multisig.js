c8361a38c4c45f2a75f537d4169b9dcf900c7c9e
Quinten De Swaef <quintendeswaef@gmail.com>
2018-05-13 15:59:10 +0200

    multisigs will come

--- web/src/main/resources/static/assets/js/custom/wallet/multisig.js
+++ web/src/main/resources/static/assets/js/custom/wallet/multisig.js
@@ -3,12 +3,22 @@
 	const address = $('#currentAddress').data('hash');
 	const multisigAddress = $('#multisigAddress').data('hash');
 
-	let multisigData = {
+	const multisigContract = CindercloudWeb3.getGlobalWeb3().eth.contract(abi.multisig).at(multisigAddress);
 
+
+	let multisigData = {
+		requiredOwners: 0,
+		owners:  []
 	};
 
 	new Vue({
 		el: '#multisigApp',
-		data: multisigData
+		data: multisigData,
+		created: function () {
+			multisigContract.m_required.call(function (err, result) {
+				multisigData.requiredOwners = result.toNumber();
+			});
+			multisigContract.
+		}
 	});
 })();
